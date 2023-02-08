54187eaf88a57c154d36bd706c254885af694ca9
Indu <Indu@DESKTOP-UQK926Q>
2023-02-08 23:12:56 +0530

    first commit

---
+++ definig_a_class/src/definig_a_class/Cricketer.java
@@ -0,0 +1,35 @@
+package definig_a_class;
+
+import definig_a_class.Cricketer;
+
+class Cricketer{
+
+String name = "Dhoni";
+
+int age = 39;
+
+String color = "Brown";
+
+String role = "Batsman";
+
+String nationality= "Indian";
+
+void Display(){
+
+System.out.println("I LOVE Criket");
+
+}
+
+public static void main(String args[]){
+
+System.out.println("main method started");
+
+Cricketer c = new Cricketer();
+
+c.Display ();
+
+System.out.println("main method ended");
+
+}
+
+}
---
+++ definig_a_class/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Indu
+ *
+ */
+module definig_a_class {
+}
